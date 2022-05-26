ff34e7a24c5bb36d48a6dce7baba6b51acaacf36
mp2it <manish.mp2it.mtr@gmail.com>
2022-05-26 11:54:09 +0530

    delete uploaded file

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,6 +1,7 @@
 const asyncHandler = require('express-async-handler')
 
 const Goal = require('../models/goalModel')
+var fs = require('fs');
 
 const getGoals = asyncHandler(async (req, res) => {
 
@@ -10,37 +11,44 @@ const getGoals = asyncHandler(async (req, res) => {
     // return console.log("hello world");
 })
 
-const setGoals =asyncHandler ( async (req, res, next) => { 
-
- 
+const setGoals = asyncHandler(async (req, res, next) => {
 
 })
 
-const updateGoals =asyncHandler(async (req, res) => {
+const updateGoals = asyncHandler(async (req, res) => {
 
-    const goal =await Goal.findById(req.params.id);
+    const goal = await Goal.findById(req.params.id);
 
-    if(!goal)
-    {
-       res.status(400)
-       throw new Error("Goal not found");
+    if (!goal) {
+        res.status(400)
+        throw new Error("Goal not found");
     }
 
-    const updatedGoal = await Goal.findByIdAndUpdate(req.params.id,req.body,{
-        new:true
+    const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, {
+        new: true
     })
 
     res.status(200).json(updatedGoal);
 })
 
-const deleteGoals =asyncHandler(async (req, res) => {
+const deleteGoals = asyncHandler(async (req, res) => {
 
-    const goal =await Goal.findById(req.params.id);
 
-    if(!goal)
-    {
-       res.status(400)
-       throw new Error("Goal not found");
+    const goal = await Goal.findById(req.params.id);
+
+    if (!goal) {
+        res.status(400)
+        throw new Error("Goal not found");
+    }
+    
+    const deleteFile = goal.image;
+    if (fs.existsSync(deleteFile)) {
+        fs.unlink(deleteFile, (err) => {
+            if (err) {
+                console.log(err);
+            }
+            console.log('deleted');
+        })
     }
 
     await goal.remove();
